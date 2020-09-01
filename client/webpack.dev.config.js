0b6b58030de6d3b76f6cfe611e6d7ff54f34ddaa
Jonty-Buchan <jontyterrence@gmail.com>
2020-09-01 22:36:55 +1200

    Adds webpack config for stages

---
+++ client/webpack.dev.config.js
@@ -0,0 +1,10 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.config.js')
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: '../server/public'
+  }
+})
